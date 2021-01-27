b83e6108bfe71adca706c7f565eb32ecbfdda58b
Sallazz <78081913+Sallazz@users.noreply.github.com>
2021-01-27 12:46:56 +0000

    Update DataSourceTest.java

--- src/Tests/DataSourceTest.java
+++ src/Tests/DataSourceTest.java
@@ -9,6 +9,7 @@ public class DataSourceTest {
 
     DataSource data = new DataSource();
 
+    // Make sure your tests' names specify what they are testing for. This way a person reading them will not have to read the whole test to understand what is happening but rather just the title.
     @Test
     public void getRates() {
         assertNotNull(data.getRates("2021-01-25"));
