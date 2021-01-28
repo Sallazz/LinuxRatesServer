78a5a268ebbb7040428f41754519a82d63f8605a
Sallazz <78081913+Sallazz@users.noreply.github.com>
2021-01-28 17:58:21 +0000

    Update ResponseInterpreter.java

--- src/data/ResponseInterpreter.java
+++ src/data/ResponseInterpreter.java
@@ -17,30 +17,30 @@ public class ResponseInterpreter {
     private List<Rate> getRatesPrivate(String response){
         //Declare new List to be returned at end of method.
         List<Rate> rates = new ArrayList<>();
-        String myString = response;
+        String myString = response;     // Why assign that to another string? And please rename from "myString" to something that describes what this holds.
 
         //Get date of response.
-        int dateStart = myString.indexOf("date") + 7;
+        int dateStart = myString.indexOf("date") + 7;           // Its better to extract "date" to a specific static var somewhere else. Also what if the myString is null? NPE. 
         String date = myString.substring(dateStart, dateStart + 10);
 
         //Remove unnecessary characters so only currencies and rates remain.
-        String s = myString.replaceAll("[^A-Z0-9.,]","");
+        String s = myString.replaceAll("[^A-Z0-9.,]",""); // NPE? Also please rename "s"
 
         //Cut string to begin where rates start - at USD.
-        int result = s.indexOf("USD");
+        int result = s.indexOf("USD");                      // Why is this singling out USD? What is the intended logic? Better parametirise if nothing else.
         if (result == -1){
             return null;
         }else{
-            String x = s.substring(result);
+            String x = s.substring(result);                 // Please rename "x".
 
             //Split each currency and create Rate object to populate in List.
             String[] curr = x.split(",");
             for (String cu : curr){
-                rates.add(new Rate(date, cu.substring(0, 3), Double.parseDouble(cu.substring(3))));
+                rates.add(new Rate(date, cu.substring(0, 3), Double.parseDouble(cu.substring(3)))); // Better assign the above to named variables and use those here. Improves readability.
             }
 
             return rates;
         }
     }
-
+// Remove blank line.
 }
